be5e725316f5ff9d2a15a1922420598e2c512564
Hug Folguera <hfolguera@gmail.com>
2021-09-27 11:46:44 +0200

    Remove cache folders

--- 04.Using_Terragrunt/eu-frankfurt-1/compartments/db_compartment/.terragrunt-cache/2DORdTqMae8Bt0bpnGmngGHPD5c/9mei15olKq9BUuyJpibfxRV3CGE/main.tf
+++
@@ -1,6 +0,0 @@
-resource "oci_identity_compartment" "this" {
-  compartment_id = var.compartment_id
-  description    = var.compartment_description
-  name           = var.compartment_name
-  enable_delete  = true
-}
--- 04.Using_Terragrunt/eu-frankfurt-1/compartments/db_compartment/.terragrunt-cache/2DORdTqMae8Bt0bpnGmngGHPD5c/9mei15olKq9BUuyJpibfxRV3CGE/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "id" {
-  description = "OCID of the created compartment"
-  value       = oci_identity_compartment.this.id
-}
